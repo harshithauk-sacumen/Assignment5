d0d9b3c3f20b49b9774dd7746ece6baec8f28555
harshithauk-sacumen <79836206+harshithauk-sacumen@users.noreply.github.com>
2021-03-03 20:43:26 +0530

    Add files via upload

---
+++ src/com/company/ThreadMainClass.java
@@ -0,0 +1,9 @@
+package com.company;
+public class MainClass{
+    public static void main(String[] args){
+        Sequence1 s1=new Sequence1();
+        s1.start();
+        Sequence2 s2=new Sequence2();
+        s2.start();
+    }
+}
