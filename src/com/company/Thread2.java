e6009d10329c08128904531d366b24b5632930f8
harshithauk-sacumen <79836206+harshithauk-sacumen@users.noreply.github.com>
2021-03-03 20:41:41 +0530

    Add files via upload

---
+++ src/com/company/Thread2.java
@@ -0,0 +1,16 @@
+package com.company;
+public class Thread2 extends Thread
+{
+    public void run()
+    {
+        for(int i=1;i<11;i++){
+            System.out.println("pass");
+            try{
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
